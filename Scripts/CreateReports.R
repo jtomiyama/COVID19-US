e37e799b81cc3967660604587ed6a4259575272b
josh-tomiyama <joshua-michael-tomiyama@uiowa.edu>
2020-08-05 03:32:41 -0500

    Initial commit of State Templates

---
+++ Scripts/CreateReports.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(openxlsx)
+library(rmarkdown)
+
+### Note this is different from RunAllAnalyses
+cleanDt <- function(x){
+  gsub("/", "_", x, fixed = TRUE)
+}
+
+cleanIdx <- function(x){
+  ifelse(x < 10, paste0("0", trimws(x)), x)
+}
+
+createTemplateConfigEnv <- function(...){
+  args <- list(...)
+  paramEnv <- new.env()
+  for (n in names(args)){
+    paramEnv[[n]] <- args[[n]]
+  }
+  return(paramEnv)
+}
+
+
+##########################################################
+date_run <- format(Sys.Date(), "%m/%d/%Y")
+date_run <- cleanDt(date_run)
+states_to_run <- c(14, 15, 16, 17, 23, 24, 
+                   26, 28, 35, 36, 42, 50)
+states_to_run <- cleanIdx(states_to_run)
+state_key <- read.xlsx("../Data/state_key.xlsx") %>% 
+  filter(state_idx %in% states_to_run)
+
+apply(state_key, 1, function(x){
+  te <- createTemplateConfigEnv(TEMPLATE_STATE_NAME = x[["state"]],
+                                TEMPLATE_STATE_IDX = x[["state_idx"]])
+  report_file_name <- paste0("../Reports/", te$TEMPLATE_STATE_IDX,"_", date_run)
+  
+  render("../Templates/R0_State_Template.Rmd", 
+         output_file = report_file_name, 
+         envir = te)
+})
