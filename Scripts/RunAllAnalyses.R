ad4bfa3e3883eac16587069904bb5815fce19b10
josh-tomiyama <joshua-michael-tomiyama@uiowa.edu>
2021-02-25 15:23:00 -0600

    Remove google mobility indices

--- Scripts/RunAllAnalyses.R
+++ Scripts/RunAllAnalyses.R
@@ -16,7 +16,7 @@ isDebug <- FALSE
 # refer to Data/state_key.xlsx
 states_to_run <- c(14, 15, 16, 17, 23, 24, 
                   26, 28, 35, 36, 42, 50)
-intervention_types_to_run <- c(1,2,3,4,5)
+intervention_types_to_run <- c(1,2,3)
 mandate_type <- "SAHO"
 templates_to_use <- c("default.template.mortality.R")
 ## Constrain ourselves to first part of the epidemic
